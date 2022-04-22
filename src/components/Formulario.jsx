d5bb50aefb10b527351a57c996bada9a7ca5cc37
rrodrigu48 <rarodriguez1525@gmail.com>
2022-04-21 23:16:13 -0500

    first

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+
+const Formulario = () => {
+    const [fruta, setFruta] = React.useState('')
+    const [descripcion, setDescripcion] = React.useState('')
+    const [lista, setLista] = React.useState([])
+
+    const guardarDatos = (e) =>{
+        e.preventDefault()
+// validar si los campos estan vacios
+        if(!fruta.trim()){
+            return alert ('Campo fruta vacio')
+        }
+
+        if(!descripcion.trim()){
+            return alert ('Campo descripcion vacio')
+        }
+//////
+        setLista ([... lista,
+            {nombreFrta: fruta, nombreDescripcion: descripcion}
+
+        ])
+    }
+
+
+    return(
+        <>
+            <h2>Formulario</h2>
+            <form onSubmit={guardarDatos}>
+                <input 
+                    type = "text"
+                    placeholder ='Ingresar fruta'
+                    onChange={(e) =>setFruta(e.target.value)}
+                />
+                <input 
+                    type = "text"
+                    placeholder ='Ingrese descripcion'
+                    onChange={(e) => setDescripcion(e.target.value)}
+                />
+                <button type = 'submin'>Agregar</button>
+
+            </form>
+            <ul>
+                {
+                    lista.map((item, index) =>(
+                        <li key={index}>
+                            <h4>{item.nombreFrta} - {item.nombreDescripcion}</h4>
+                        </li>
+                    ) )
+                }
+            </ul>
+
+        </>
+
+    )
+}
+
+export default Formulario
